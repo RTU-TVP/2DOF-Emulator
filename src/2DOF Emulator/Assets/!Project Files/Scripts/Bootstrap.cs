0db19f5018afaa219f23393e0543e3f7c199c1f0
ShutovKS <schutow.kir@yandex.ru>
2024-10-23 00:49:19 +0300

    Add Bootstrap

---
+++ src/2DOF Emulator/Assets/!Project Files/Scripts/Bootstrap.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bootstrap : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
