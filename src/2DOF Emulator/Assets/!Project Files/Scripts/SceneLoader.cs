7f6550b2867275dfd0beb208cca48a628e238afe
ShutovKS <schutow.kir@yandex.ru>
2024-10-23 01:31:24 +0300

    Add scene loader

---
+++ src/2DOF Emulator/Assets/!Project Files/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+
+    private AsyncOperation _sceneAsync;
+
+    private void Start()
+    {
+        _sceneAsync = SceneManager.LoadSceneAsync(sceneName);
+        if (_sceneAsync != null)
+        {
+            _sceneAsync.allowSceneActivation = false;
+        }
+        else
+        {
+            Debug.LogError($"SceneLoader: Scene '{sceneName}' not found.");
+        }
+    }
+
+    public void LoadScene()
+    {
+        _sceneAsync.allowSceneActivation = true;
+    }
+}
